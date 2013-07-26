2c712e4302151bedfc81ac0301d415a6b9c873df
Brent Lee <blee1555@gmail.com>
2013-07-26 00:46:16 -0600

    added signIn and createAccount templates

--- js/router.js
+++ js/router.js
@@ -1,6 +1,8 @@
 (function(App){
 	App.Router.map(function(){
 		this.route('about');
+		this.route('signIn');
+		this.route('createAccount');
 		this.resource('decks', function(){
 			this.route('createDeck');
 			this.resource('deck', {path:":deck_id"});
